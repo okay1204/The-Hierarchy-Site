ec60064b3aa49efeb3728b99a621266096c564b7
okayyyy <zghan2005@gmail.com>
2021-01-29 23:58:42 -0800

    Redesigned home page

---
+++ src/components/textbox.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class TextBox extends React.Component {
+    
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,9 @@
 import '../styles/home.css'
+
 import BannerRight from '../images/banner right.png'
 import BannerMiddle from '../images/banner middle.png'
 import BannerLeft from '../images/banner left.png'
-
-const features_list = [
-    'An interactive tutorial to help you get started',
-    'Working with custom mini games',
-    'Stealing from others, with a risk of choosing to steal more money',
-    'A bank, to keep your money safe (or not?)',
-    'Heists, that require multiple people to do so',
-    'Games, for something fun to do with a reward for winning as well',
-    'Gambling, to bet your money and potentially win or lose it all',
-    'Fees and taxes, that are collected at a certain interval',
-    'A shop, with stocks built into it for constantly changing prices',
-    'Items, to assist you with getting more money',
-    'Jail, along with bailing for some sort of punishment for crime',
-    'Self assignable roles so people can get to know you',
-    'Daily rewards that get better every day',
-    'A wallet dropping system for competitiveness to claim the wallet first',
-    'Gangs, to have groups compete against each other'
-]
+import PlaceHolder from '../images/placeholder.ico'
 
 function Home() {
     return(
@@ -37,27 +21,11 @@ function Home() {
 
             <hr className="banner-divider"/>
 
-            <div className="text-box">
-                <strong>This server is one of a kind, I promise you</strong>
-                <br/><br/>
-                The Hierarchy is an economy server whose sole purpose is economy, and nothing else! 
-                Ran by a ➡️ <strong>custom bot</strong> ⬅️!
-                <br/><br/>
-
-                <u>Some features are</u>
-                <br/><br/>
-
-                <ul className="featured-list">
-                    {features_list.map(featured => (
-                        <li>{featured}</li>
-                    ))}
-                </ul>
-
-                <br/><br/>
-                And so much more! There are always new updates so you will *never* get bored!
-                <br/><br/>
-
-                <em>So, you do think you have what it takes to become on top of The Hierarchy? Find out now!</em>
+            <div className="left-text-box">
+                <div>
+                    <img src={PlaceHolder} alt="placeholder"></img>
+                    <span>An economy server taken to a whole new level</span>
+                </div>
             </div>
 
 
