fb252d641d6c6f9af0a3d4cec9845aa7000d9eca
okayyyy <zghan2005@gmail.com>
2021-01-30 14:20:02 -0800

    renamed ad-box classname to persuasive-box because adblocker is stupid

--- src/components/adbox.js
+++ src/components/adbox.js
@@ -3,7 +3,7 @@ import '../styles/adbox.css'
 function AdBox({children, pos}) {
 
     return (
-        <div className={`ad-box ${pos === 'left' ? 'ad-box-left' : 'ad-box-right'}`}>
+        <div className={`persuasive-box ${pos === 'left' ? 'persuasive-box-left' : 'persuasive-box-right'}`}>
             {children}
         </div>
     )
