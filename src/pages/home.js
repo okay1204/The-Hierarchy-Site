4f45b7decae89a37e3134bee396f38119d35bce1
okayyyy <zghan2005@gmail.com>
2021-01-29 22:35:55 -0800

    Fixed all isues on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,17 +3,36 @@ import BannerRight from '../images/banner right.png'
 import BannerMiddle from '../images/banner middle.png'
 import BannerLeft from '../images/banner left.png'
 
+const features_list = [
+    'An interactive tutorial to help you get started',
+    'Working with custom mini games',
+    'Stealing from others, with a risk of choosing to steal more money',
+    'A bank, to keep your money safe (or not?)',
+    'Heists, that require multiple people to do so',
+    'Games, for something fun to do with a reward for winning as well',
+    'Gambling, to bet your money and potentially win or lose it all',
+    'Fees and taxes, that are collected at a certain interval',
+    'A shop, with stocks built into it for constantly changing prices',
+    'Items, to assist you with getting more money',
+    'Jail, along with bailing for some sort of punishment for crime',
+    'Self assignable roles so people can get to know you',
+    'Daily rewards that get better every day',
+    'A wallet dropping system for competitiveness to claim the wallet first',
+    'Gangs, to have groups compete against each other'
+]
 
 function Home() {
     return(
-        <div className="homeBody">
+        <div className="home-body">
             <div className="banner">
                 <img src={BannerLeft} alt="Hierarchy Banner"/>
-                <img className="bannerMiddle" src={BannerMiddle} alt="Hierarchy Banner"/>
+                <img className="banner-middle" src={BannerMiddle} alt="Hierarchy Banner"/>
                 <img src={BannerRight} alt="Hierarchy Banner"/>
             </div>
 
-            <div className="textBox">
+            <hr className="banner-divider"/>
+
+            <div className="text-box">
                 <strong>This server is one of a kind, I promise you</strong>
                 <br/><br/>
                 The Hierarchy is an economy server whose sole purpose is economy, and nothing else! 
@@ -23,25 +42,11 @@ function Home() {
                 <u>Some features are</u>
                 <br/><br/>
 
-                <div className="featuredList">
-                    <ul>
-                        <li>An interactive tutorial to help you get started<br/></li>
-                        <li>Working with custom mini games<br/></li>
-                        <li>Stealing from others, with a risk of choosing to steal more money<br/></li>
-                        <li>A bank, to keep your money safe <em>(or not?)</em><br/></li>
-                        <li>Heists, that require multiple people to do so<br/></li>
-                        <li>Games, for something fun to do with a reward for winning as well<br/></li>
-                        <li>Gambling, to bet your money and potentially win or lose it all<br/></li>
-                        <li>Fees and taxes, that are collected at a certain interval<br/></li>
-                        <li>A shop, with stocks built into it for constantly changing prices<br/></li>
-                        <li>Items, to assist you with getting more money<br/></li>
-                        <li>Jail, along with bailing for some sort of punishment for crime<br/></li>
-                        <li>Self assignable roles so people can get to know you<br/></li>
-                        <li>Daily rewards that get better every day<br/></li>
-                        <li>A wallet dropping system for competitiveness to claim the wallet first<br/></li>
-                        <li>Gangs, to have groups compete against each other<br/></li>
-                    </ul>
-                </div>
+                <ul className="featured-list">
+                    {features_list.map(featured => (
+                        <li>{featured}</li>
+                    ))}
+                </ul>
 
                 <br/><br/>
                 And so much more! There are always new updates so you will *never* get bored!
@@ -49,6 +54,7 @@ function Home() {
 
                 <em>So, you do think you have what it takes to become on top of The Hierarchy? Find out now!</em>
             </div>
+
         </div>
     )
 }
