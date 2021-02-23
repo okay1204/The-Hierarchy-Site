0f320f25c253afe63f3043d65bdf396dc52ec94f
okayyyy <zghan2005@gmail.com>
2021-02-23 09:24:56 -0800

    Made footer point directly to discord server invite

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className='footer'>
             <wbr />
-            <a className='join-page-button' href='/join'>Join Now!</a>
+            <a className='join-page-button' href='https://discord.gg/pZXFsSETnx'>Join Now!</a>
         </div>
     )
 }
