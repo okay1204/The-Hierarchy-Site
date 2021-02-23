644c5c33085a03fb548b244fd75a3bd67e1692f5
okayyyy <zghan2005@gmail.com>
2021-02-23 14:18:47 -0800

    Added Stats home page

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,9 +1,27 @@
 import '../styles/stats.css'
 
 function Stats() {
+
+
     return(
-        <div>
-            stats
+        <div id='stats-body'>
+            
+            <div id='stats-sections'>
+
+                <div className='section-1'>
+                    <a href='/stats'>Members</a>
+                </div>
+
+                <div className='section-2'>
+                    <a href='/stats'>Gangs</a>
+                </div>
+
+                <div className='section-3'>
+                    <a href='/stats'>Awards</a>
+                </div>
+
+            </div>
+
         </div>
     )
 }
