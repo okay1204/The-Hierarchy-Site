4e2a0c65f06f4d8a1dfbfd4096cb81abdc603959
okayyyy <zghan2005@gmail.com>
2021-02-23 09:19:17 -0800

    Added more info to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,8 +12,13 @@ import MoneyBag from '../images/money bag.png'
 import Crowd from '../images/crowd.png'
 import Strategy_Board from '../images/strategy board.png'
 
+import Tutorial from '../images/tutorial.png'
 import Shop from '../images/shop.png'
 import Universities from '../images/universities.png'
+import Jobs from '../images/jobs.png'
+import Fees from '../images/fees.png'
+import Gang from '../images/gang.png'
+import Checklist from '../images/checklist.png'
 
 
 function Home() {
@@ -69,6 +74,16 @@ function Home() {
 			<hr className='home-divider' />
 			
 			<ExplainSection reverse={true}>
+				<img src={Tutorial} alt='The shop on the discord server'/>
+				<div>
+					<h2>Interactive Tutorial</h2>
+					<p>An interactive tutorial for a quickstart of how to play!</p>
+				</div>
+			</ExplainSection>
+
+			<hr className='home-divider' />
+			
+			<ExplainSection reverse={false}>
 				<img src={Shop} alt='The shop on the discord server'/>
 				<div>
 					<h2>Complex Shop System</h2>
@@ -78,13 +93,55 @@ function Home() {
 
 			<hr className='home-divider' />
 
-			<ExplainSection reverse={false}>
-				<img src={Universities} alt='The shop on the discord server'/>
+			<ExplainSection reverse={true}>
+				<img src={Universities} alt='Different universities on the discord server'/>
 				<div>
 					<h2>Many Universities</h2>
 					<p>Lots of Universities to study at to achieve different majors. Pass the final and get a new job!</p>
 				</div>
 			</ExplainSection>
+
+			<hr className='home-divider' />
+
+			<ExplainSection reverse={false}>
+				<img src={Jobs} alt='Different jobs on the discord server'/>
+				<div>
+					<h2>Fun Jobs</h2>
+					<p>Many Jobs each with their own amount of pay, cooldown, and difficulty. They all come with their own little minigames to play for cash!</p>
+				</div>
+			</ExplainSection>
+
+			<hr className='home-divider' />
+
+			<ExplainSection reverse={true}>
+				<img src={Fees} alt='Fee timers on the discord server'/>
+				<div>
+					<h2>Daily/Bi-Daily Fees</h2>
+					<p>Taxes and Bank Fees that are payed automatically. Be sure to make more than you lose each day!</p>
+				</div>
+			</ExplainSection>
+
+			<hr className='home-divider' />
+
+			<ExplainSection reverse={false}>
+				<img src={Gang} alt='The Formula gang on the discord server'/>
+				<div>
+					<h2>Fully Customizable Gangs</h2>
+					<p>Group up with other members of the server, join one or create your own! Fully customizable with the ability to set an icon, color, name, description, role, and more!</p>
+				</div>
+			</ExplainSection>
+
+			<hr className='home-divider' />
+
+			<ExplainSection reverse={true}>
+				<img src={Checklist} alt='More Symbol'/>
+				<div>
+					<h2>..much, much more!</h2>
+					<p>This bot is jam-packed with features, feel free to discover them for yourselves!</p>
+				</div>
+			</ExplainSection>
+
+
 
 		</div>
 	)
