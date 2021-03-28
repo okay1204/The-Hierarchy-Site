6a599bfeaad2c1ca2858ce10406e179dbbdae29a
okayyyy <zghan2005@gmail.com>
2021-03-27 19:19:19 -0700

    Progress ig

--- src/components/memberpreview.js
+++ src/components/memberpreview.js
@@ -21,23 +21,28 @@ function MemberPreview(props) {
 
     return (
         <a className={`member-preview ${props.boosting ? 'premium-border' : ''}`} href={`/stats/members/${props.id}`}>
-            <div className='img-wrapper'>
 
-                <img src={props.avatar_url} className='avatar' alt='Profile pic' />
-                <img className='status' src={status_key[props.status]} alt={props.status}/>
-                
-            </div>
-            <div className='name-wrapper'>
-                <div className='main-name'>
-                    <span className='name'>{props.name}</span>
-                    <span className='discriminator'>#{props.discriminator}</span>
+            <div className='left'>
+                <div className='img-wrapper'>
+
+                    <img src={props.avatar_url} className='avatar' alt='Profile pic' />
+                    <img className='status' src={status_key[props.status]} alt={props.status}/>
+                    
                 </div>
-                {props.nick && <span className='nick'>{props.nick}</span>}
+                <div className='name-wrapper'>
+                    <div className='main-name'>
+                        <span className='name'>{props.name}</span>
+                        <span className='discriminator'>#{props.discriminator}</span>
+                    </div>
+                    {props.nick && <span className='nick'>{props.nick}</span>}
+                </div>
+                
+                {props.boosting && <span className='boosting'>Premium</span>}
             </div>
-
-            {preview_stat}
             
-            {props.boosting && <span className='boosting'>Premium</span>}
+            <div className='right'>
+                {preview_stat}
+            </div>
         </a>
     )
 }
